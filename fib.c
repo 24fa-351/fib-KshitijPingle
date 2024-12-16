6a7aa1df02078e2c29e36438f2fce6cc17f9d9ad
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2024-12-15 16:52:32 -0800

    Formatted this file

--- fib.c
+++ fib.c
@@ -1,59 +1,65 @@
-//Kshitij Pingle
-//CWID: 885626978
-//CPSC 351
-//Assignment 1 - Fibonacci Sequence
-//5 September 2024
+// Kshitij Pingle
+// CWID: 885626978
+// CPSC 351
+// Assignment 1 - Fibonacci Sequence
+// 5 September 2024
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef unsigned int (*FibFuncPtr) (unsigned int n);
+typedef unsigned int (*FibFuncPtr)(unsigned int max);
 
-//Recursive Fibonacci Sequence
-unsigned int fib_recurse(unsigned int n) {
+// Recursive Fibonacci Sequence
+unsigned int fib_recurse(unsigned int max)
+{
 
-   if (n < 2) {
-      return n;
+   if (max < 2)
+   {
+      return max;
    }
-   return fib_recurse(n - 1) + fib_recurse(n - 2);
+   return fib_recurse(max - 1) + fib_recurse(max - 2);
 }
 
-//Iterative Fibonacci Sequence
-unsigned int fib_iterate(unsigned int n) {
-      
+// Iterative Fibonacci Sequence
+unsigned int fib_iterate(unsigned int n)
+{
+
    unsigned int temp = 0;
    unsigned int previous = 0;
    unsigned int current = 0;
-   
-   for (unsigned int i = 1; i < n; ++i) {     //Sorry prof, I like my 'i' counter vars
+
+   for (unsigned int ix = 1; ix < n; ++ix)
+   {
       temp = previous;
       previous = current;
       current += temp;
-      
-      if (i == 1) {
-         current++;     //Update current on first iteration
+
+      if (ix == 1)
+      {
+         current++; // Update current on first iteration
       }
-      
    }
 
    return current;
 }
 
+int main(unsigned int argc, char *argv[])
+{
 
-int main(unsigned int argc, char *argv[]) {
-
-   if (argc != 4) {
+   if (argc != 4)
+   {
       printf("Invalid number of arguments\n");
       printf("Usage: ./fib <num1> <fib_method> <file>\n");
       return 1;
    }
 
-   //Receive and process all passed arguments
+   // Receive and process all passed arguments
    unsigned int num1 = atoi(argv[1]);
 
-   char fib_method = argv[2][0];           //"i" = iterative, "r" = recursive
+   char fib_method = argv[2][0]; // "i" = iterative, "r" = recursive
 
-   if ((fib_method != 'i') && (fib_method != 'r')) {
+   if ((fib_method != 'i') && (fib_method != 'r'))
+   {
       printf("Invalid method. Fib method should either be an 'i' or a 'r'.\n");
       printf("Usage: ./fib <num1> <fib_method> <file>\n");
       return 1;
@@ -61,40 +67,38 @@ int main(unsigned int argc, char *argv[]) {
 
    unsigned int num2 = 0;
 
-   FILE *file = fopen(argv[3], "r");       //"r" = read
+   FILE *file = fopen(argv[3], "r"); // "r" = read
 
-   fscanf(file, "%u", &num2);            //Get single num from file
+   fscanf(file, "%u", &num2); // Get single num from file
    fclose(file);
 
    unsigned int max = num1 + num2;
 
    FibFuncPtr fib_routine;
 
-   //Fibonnaci Sequence
-      // 0 1 1 2 3 5 8 13 21 34 55 ...
+   // Fibonnaci Sequence
+   // 0 1 1 2 3 5 8 13 21 34 55 ...
 
-   switch (fib_method) {
-      case 'i':
-         //Call Iterative Fibonacci Function
-         // fib_num = fib_iterate(previous, current, max);
-         fib_routine = fib_iterate;
-         break;
+   switch (fib_method)
+   {
+   case 'i':
+      // Call Iterative Fibonacci Function
+      fib_routine = fib_iterate;
+      break;
 
-      case 'r':
-         //Call Recursive Fibonacci Function
-         //fib_num = fib_recurse_provider(max - 1);
-         // fib_num = fib_recurse(max - 1);
-         fib_routine = fib_recurse;
-         --max;
-         break;
-   
-      default:
-         break;
+   case 'r':
+      // Call Recursive Fibonacci Function
+      fib_routine = fib_recurse;
+      --max;
+      break;
+
+   default:
+      break;
    }
 
    unsigned int fib_num = fib_routine(max);
 
-   printf("%u", fib_num);       //'u' means unsigned int
+   printf("%u", fib_num); // 'u' means unsigned int
 
    return 0;
 }
